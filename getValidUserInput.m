78c96f007e8ff59e28765ebf229b8b798184890d
stear149 <stear149@umn.edu>
2025-11-17 17:31:05 -0600

    Update getValidUserInput.m

--- getValidUserInput.m
+++ getValidUserInput.m
@@ -8,11 +8,25 @@ function [G, C, F, R, T, filename] = getValidUserInput()
         end
     end
     
-    G = readmatrix(filename, 'Sheet','G');
-    C = readmatrix(filename, 'Sheet','C');
-    F = readmatrix(filename, 'Sheet','F');
-    R = readmatrix(filename, 'Sheet','R');
-    T = readmatrix(filename, 'Sheet','T');
+        G = readmatrix(filename, 'Sheet','G');
+        C = readmatrix(filename, 'Sheet','C');
+        F = readmatrix(filename, 'Sheet','F');
+        R = readmatrix(filename, 'Sheet','R');
+        T = readmatrix(filename, 'Sheet','T');
+
+        assert(size(G, 1) > 3, 'G must have more than 3 rows');
+        assert(size(G, 2) == 2, 'G must have 2 cols');
+        assert(all(pdist(G)) ~= 0);
+
+        assert(size(C, 1) > 3, 'C must have more than 3 rows');
+        assert(size(C, 2) == 2, 'C must have 2 cols');
+
+    % try 
+    % 
+    % catch
+    %     error('Error reading the file. Please check the format and try again.');
+    % end
+    
 
 
 end
