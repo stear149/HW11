c4f1428c9507d7a87c1b50f0ea0d7f04149539a7
stear149 <stear149@umn.edu>
2025-11-24 21:27:07 -0600

    Update drawNode.m

--- drawNode.m
+++ drawNode.m
@@ -1,3 +1,25 @@
+%==========================================================================
+% function drawNode(xc, yc, radius, label, fontsize)
+%
+% This function draws discs where its told to.
+%
+% Input Arguments:
+%   xc - Center in the X direction
+%   yc - Center in the Y direction
+%   radius - Radius to draw the disk
+%   label - Name of the node
+%   fontsize - how big the font it, normalized to the graph
+%
+% Output Arguments:
+%   None
+%
+% Authors: Group I
+%   Owen Haberstroh (haber297@umn.edu)
+%   Lily Wilkerson (wilke541@umn.edu)
+%   Evan Stearns (stear149@umn.edu)
+%
+% Version: Nov. 24, 2025
+%==========================================================================
 function drawNode(xc, yc, radius, label, fontsize)
 
 
